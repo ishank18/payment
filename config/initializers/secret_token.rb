5e9ab6d486665507910d226bb0256de4c453ac50
Ishank Gupta <ishank.gupta@vinsol.com>
2013-01-21 15:14:48 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Payment::Application.config.secret_token = '6857b39d14668b70f12d74086784ec373b1ddfc2a73923a3118d77e5868c66eee5815b9d701549968c27b0eb2e5d74dad169d8bebc891656779be225faaa7042'
